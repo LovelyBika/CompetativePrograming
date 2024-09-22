a02c2a25811fbbc9963f108fb206fae782eb62ba
LovelyBika <iamlovely.dev@gmail.com>
2024-09-22 20:35:53 +0530

    Now everything is fine

--- JAVA-assignment1-master/Java Basic/fibonacci.java
+++ JAVA-assignment1-master/Java Basic/fibonacci.java
@@ -1,11 +1,14 @@
-public class fibonacci{
-    public static void main(String[] args){
-        int n=7;
+public class fibonacci {
+    public static void main(String[] args) {
+        int n = 9;
         System.out.println(fibonacci(n));
     }
-    private static long fibonacci(int n){
-        if(n<=1){
-        return n;
+
+    private static long fibonacci(int n) {
+        if (n <= 1) {
+            return n;
+        }
+
+        return (fibonacci(n - 1) + fibonacci(n - 2));
     }
-        return (fibonacci(n-1)+fibonacci(n-2));
-    }}
+}
